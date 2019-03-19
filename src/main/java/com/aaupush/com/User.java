6a6c45366147c39a8e20e781534552adc75b31c1
Kaleb Takele <kaleb.takele0810@gmail.com>
2019-03-19 09:58:38 +0300

    JAXB replaced by hibernate

---
+++ src/main/java/com/aaupush/com/User.java
@@ -0,0 +1,55 @@
+package com.aaupush.com;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+	private String Firstname;
+	private String Lastname;
+	private String Email;
+	private String Password;
+	@Id
+	private int Id;
+
+	public String getFirstname() {
+		return Firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		Firstname = firstname;
+	}
+
+	public String getLastname() {
+		return Lastname;
+	}
+
+	public void setLastname(String lastname) {
+		Lastname = lastname;
+	}
+
+	public String getEmail() {
+		return Email;
+	}
+
+	public void setEmail(String email) {
+		Email = email;
+	}
+
+	public String getPassword() {
+		return Password;
+	}
+
+	public void setPassword(String password) {
+		Password = password;
+	}
+
+	public int getId() {
+		return Id;
+	}
+
+	public void setId(int id) {
+		Id = id;
+	}
+}
