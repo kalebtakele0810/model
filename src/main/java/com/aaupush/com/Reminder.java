f99eeb11d4d9cd30d9d0a90e930341a23e8e89bc
amanshimeles <amanuelshims@gmail.com>
2019-04-27 10:48:02 -0700

    Reminder model added.

--- src/main/java/com/aaupush/com/Reminder.java
+++ src/main/java/com/aaupush/com/Reminder.java
@@ -1,5 +1,31 @@
 package com.aaupush.com;
 
+import java.util.Date;
+
+import javax.persistence.Entity;
+
+@Entity
 public class Reminder {
+private String title;
+private Date duedate;
+private String place;
+public String getTitle() {
+	return title;
+}
+public void setTitle(String title) {
+	this.title = title;
+}
+public Date getDuedate() {
+	return duedate;
+}
+public void setDuedate(Date duedate) {
+	this.duedate = duedate;
+}
+public String getPlace() {
+	return place;
+}
+public void setPlace(String place) {
+	this.place = place;
+}
 
 }
